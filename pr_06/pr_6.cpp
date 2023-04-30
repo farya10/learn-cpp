d96f2ef454dfe54718cd62605543726e712a2690
farya <fdash@yandex.ru>
2023-05-01 00:50:16 +0300

    add var 4

--- pr_06/pr_6.cpp
+++ pr_06/pr_6.cpp
@@ -1,20 +1,26 @@
-﻿// pr_6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
+
+class A { public: void f() { cout << " A"; } };
+
+class C : public virtual A { public: void f() { cout << " C"; } };
+
+class B : public virtual A, public virtual C { public: void f() { cout << " B"; } };
+
+class E : public virtual C { public: void f() { cout << " E"; } };
+
+class D : public virtual B, public virtual E { public: void f() { cout << " D"; } };
+
+class F : public virtual D, public virtual E { public: void f() { cout << " F"; }; };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    F object; object.f();
+    object.A::f();
+    object.B::f();
+    object.C::f();
+    object.D::f();
+    object.E::f();
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
